c7ee35f4b3cd29286bbb8423a8909059640e38f6
sriman001 <110296786+sriman001@users.noreply.github.com>
2022-11-18 21:16:51 +0530

    Create EulerTotient.java

---
+++ EulerTotient.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class EulerTotient {
+   public static int gcd(int a,int b){
+      int i, hcf = 0;
+         for(i = 1; i <= a || i <= b; i++) {
+            if( a%i == 0 && b%i == 0 )
+            hcf = i;
+         }
+         return hcf;
+   }
+   public static void main(String args[]){
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter the n value :");
+      int n = sc.nextInt();
+     
+      for (int i = 1; i <= n; i++){
+         int x = 1;
+         for (int j = 2; j < i; j++){
+            if (gcd(j, n) == 1){
+               x++;
+            }
+         }
+         System.out.println(x);
+      }
+   }
+}
